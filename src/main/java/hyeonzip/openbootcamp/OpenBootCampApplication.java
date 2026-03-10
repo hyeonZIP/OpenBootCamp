ba5c3d54d61aaeaf4e8b7d984ff51ffcbde74e51
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2026-03-10 13:08:42 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/hyeonzip/openbootcamp/OpenBootCampApplication.java
@@ -0,0 +1,13 @@
+package hyeonzip.openbootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenBootCampApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenBootCampApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hyeonzip/openbootcamp/OpenBootCampApplicationTests.java
@@ -0,0 +1,13 @@
+package hyeonzip.openbootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenBootCampApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
