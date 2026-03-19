bd68f2fac9c42184191413c1c0c7650bb8d90844
hyeonZIP <aminez4466@gmail.com>
2026-03-20 01:21:58 +0900

    feat: UserRequest DTO 추가

---
+++ src/main/java/hyeonzip/openbootcamp/user/dto/UserRequest.java
@@ -0,0 +1,28 @@
+package hyeonzip.openbootcamp.user.dto;
+
+import hyeonzip.openbootcamp.common.enums.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+public record UserRequest(
+    @NotBlank(message = "GitHub ID는 필수입니다.")
+    String githubId,
+
+    @NotBlank(message = "사용자명은 필수입니다.")
+    @Length(max = 50, message = "사용자명은 50자 이하여야 합니다.")
+    String username,
+
+    @Email(message = "이메일 형식이 올바르지 않습니다.")
+    String email,
+
+    @URL
+    String avatarUrl,
+
+    @NotNull(message = "역할은 필수입니다.")
+    Role role
+) {
+
+}
